c7dbd3af1f56787b0f831a814108f06e875f8a12
Ed <edwoulfe@gmail.com>
2023-10-12 16:48:59 -0400

    Testing env file and gitignore.

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,4 +1,10 @@
 export default function SearchBar() {
+  function searchByKeyword() {
+    let results = YouTube.Search.list("id,snippet", { q: "dogs", maxResults: 25 });
+    for (let i in results.items) {
+      let item = results.items[i];
+    }
+  }
   return (
     <div className="search-container">
       <input type="search" id="search__input" name="q" />
