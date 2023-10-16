d56fe3252a6d7786390d7e15def2dab4ce0c96aa
Ed <edwoulfe@gmail.com>
2023-10-15 22:11:50 -0400

    Search bar fuctionality added, implemented youtube video search API.

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,14 +1,37 @@
 export default function SearchBar() {
-  function searchByKeyword() {
-    let results = YouTube.Search.list("id,snippet", { q: "dogs", maxResults: 25 });
-    for (let i in results.items) {
-      let item = results.items[i];
-    }
+  // async function submitSearch(keyword: string) {
+  //   const encodedString = encodeURI(keyword + " ambience");
+  //   const youtubeURL =
+  //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=25&order=relevance&q=${encodedString}&type=video&videoDuration=any&videoEmbeddable=true&videoLicense=any&videoType=videoTypeUnspecified&key=` +
+  //     import.meta.env.VITE_YOUTUBE_API_KEY;
+  //   console.log(keyword);
+
+  //   FormData;
+  //   const response = await fetch(youtubeURL);
+  //   const videos = await response.json();
+
+  //   console.log(videos);
+  // }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const target = e.currentTarget;
+
+    console.log(target.searchTerm.value);
   }
+
   return (
     <div className="search-container">
-      <input type="search" id="search__input" name="q" />
-      <button>Search</button>
+      <form method="post" onSubmit={handleSubmit}>
+        <label>
+          Text input:{" "}
+          <input
+            name="searchTerm"
+            placeholder="Search with keywords separated by commas:  Snow, Lord of the Rings, Calm"
+          />
+        </label>
+        <button type="submit">Submit form</button>
+      </form>
     </div>
   );
 }
