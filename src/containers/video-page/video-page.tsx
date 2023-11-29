8e8b823eb3c60b1884774d5fc69b0cd02b1ffa3d
Ed <edwoulfe@gmail.com>
2023-11-29 17:06:09 -0500

    Added a pause and play button to the focus mode overlay so the user doesnt have to click focus mode toggle just to pause/play their video every time they need to step away.

--- src/containers/video-page/video-page.tsx
+++ src/containers/video-page/video-page.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-import YouTube, { YouTubeProps } from "react-youtube";
+import YouTube, { YouTubeProps, YouTubePlayer } from "react-youtube";
 import { YoutubeVideoDetailsObject } from "../../interfaces/video-details";
 import "./video-page.scss";
 
@@ -12,8 +12,10 @@ interface VideoPageProps {
 export default function VideoPage(props: { focusModeData: VideoPageProps }) {
   const location = useLocation();
   const locationState = location.state as YoutubeVideoDetailsObject;
-  const [video, setVideo] = useState<YoutubeVideoDetailsObject>(locationState);
   const { focusModeActive, setFocusModeActive } = props.focusModeData;
+
+  const [videoElement, setVideoElement]: YouTubePlayer = useState();
+  const [video, setVideo] = useState<YoutubeVideoDetailsObject>(locationState);
 
   useEffect(() => {
     if (locationState === null || locationState === undefined) {
@@ -34,10 +36,20 @@ export default function VideoPage(props: { focusModeData: VideoPageProps }) {
     setVideo(videoInfo.items[0]);
   }
 
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+  const onPlayerReady = (event: YouTubePlayer) => {
     // access to player in all event handlers via event.target
-    event.target.pauseVideo();
+    setVideoElement(event);
   };
+
+  function togglePlayer(player: YouTubePlayer) {
+    // playerStatus comes in as a number, if it is 1 it means the player is currently playing, if 2 the player is currently paused
+    const playerStatus = player.data;
+    if (playerStatus === 1) {
+      videoElement.target.pauseVideo();
+    } else {
+      videoElement.target.playVideo();
+    }
+  }
 
   const opts: YouTubeProps["opts"] = {
     height: "400",
@@ -47,14 +59,29 @@ export default function VideoPage(props: { focusModeData: VideoPageProps }) {
       origin: "https://www.youtube.com",
     },
   };
+  useEffect(() => {
+    if (videoElement !== undefined) {
+      console.log(videoElement.data);
+    }
+  }, [videoElement]);
 
   return (
     <div className="video-page-container">
       {video !== undefined ? (
         <>
           <h3>{video?.snippet.title}</h3>
-          <YouTube videoId={video?.id} opts={opts} onReady={onPlayerReady} />
-          <div className="focus-mode-container">
+          <YouTube
+            videoId={video?.id}
+            opts={opts}
+            onReady={onPlayerReady}
+            onStateChange={(event) => setVideoElement(event)}
+          />
+          {focusModeActive ? (
+            <button className="focus-mode__button" onClick={() => togglePlayer(videoElement)}>
+              {videoElement.data !== undefined && videoElement.data === 1 ? <>Pause</> : <>Play</>}
+            </button>
+          ) : null}
+          <div className="focus-mode__container">
             {focusModeActive ? <p>Focused</p> : <p>Enable Focus Mode</p>}
             <label className="switch">
               <input
